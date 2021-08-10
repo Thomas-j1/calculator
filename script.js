b75bb5cd92cebc931f365f8913b3244f3951031e
Thomas-j1 <Thomas.johanness1@gmail.com>
2021-08-10 20:58:44 +0200

    Edited index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 
+const numberButtons = document.querySelectorAll('.number');
+const display = document.querySelector('#display');
+
+//operator buttons
+const addButton = document.querySelector('#add');
+const clearButton = document.querySelector('#clear');
+
+
+let displayValue = "";
+let firstValue;
+let secondValue;
 
 //add
 
@@ -11,3 +22,24 @@
 function operate(operator, x, y){
 
 }
+
+function numberBtnClick(){
+    let n = this.value;
+    displayValue+=n;
+    updateDisplay();
+}
+
+function updateDisplay(){
+    display.innerHTML=displayValue;
+}
+
+//eventListeners
+
+clearButton.addEventListener('click', () => {
+    displayValue="";
+    updateDisplay();
+})
+
+numberButtons.forEach(button => 
+    button.addEventListener('click', numberBtnClick));
+
